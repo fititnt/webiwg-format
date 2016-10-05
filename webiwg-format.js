634f53c26780e77cd7c23d85c72b6a7ee1a25aca
Emerson Rocha Luiz <emerson@alligo.com.br>
2016-10-05 00:36:43 -0300

    Basic working; At least does not trow exceptions

--- webiwg-format.js
+++ webiwg-format.js
@@ -11,23 +11,54 @@ function IWG(input) {
 
   for (let i = 0; i < lines.length; i++) {
     if (i + 1 < lines.length) {
+      //console.log(lines);
+      console.log('     >IWG loop<', lines[i], lines[i + 1]);
       [lines[i], lines[i + 1]] = mesclaLinhas(lines[i], lines[i + 1]);
     }
 
 
-    console.log(lines[i]);
-    console.log(">", frases(lines[i]));
+    //console.log(lines[i]);
+    //console.log(">", frases(lines[i]));
   }
 
   result = lines.join("\r\n");
   return result;
 }
 
+/**
+ * Split a string in a array, by dots
+ * @param  {String} line
+ * @return {Array}
+ */
 function frases(line) {
-  var frases = line.split(". "), result;
+  //console.log('     >frases loop<', line);
+  var frases = line.split ? line.split(". ") : line[0].split(". "); // POG
+  //console.log('     >frases loop fim<', frases);
+  if (frases.length > 1) {
+    frases[0] = frases[0] + ".";
+    // @todo resolver demais casos
+  }
   return frases;
 }
 
+/**
+ * Try to merge or split lines by final dot
+ */
 function mesclaLinhas(line1, line2) {
-  return [line1, line2];
+  var frasesLine1 = frases(line1), frasesLine2 = frases(line2), temp;
+
+  for (let i = 0; i < frasesLine1.length; i++) {
+    console.log('     >mesclaLinhas loop<', frasesLine1[i].length, frasesLine1[i]);
+    if (frasesLine1[i].length < 40) {
+      if (frasesLine1.shift && frasesLine2.unshift) { // POG!
+        temp = frasesLine1.shift();
+        //frasesLine2.unshift(frasesLine1);
+        frasesLine2 = frasesLine1.concat(frasesLine2);
+        frasesLine1 = [temp];
+        console.log(frasesLine1, frasesLine2);
+      }
+
+    }
+  }
+  return [frasesLine1, frasesLine2];
 }
